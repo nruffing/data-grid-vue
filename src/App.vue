c8798d6c65ef0c47fc8dd94b7219400b0f4d435d
Nicholas Ruffing <nicholasruffing70@gmail.com>
2023-01-12 19:36:40 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <main>
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App'
+})
+</script>
+
+<style>
+body, html {
+  margin: 0;
+}
+main {
+  height: 100vh;
+  width: 100vw;
+  background-color: black;
+}
+</style>
