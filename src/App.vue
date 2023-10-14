a0bff2f8b6d367beab5ea6b0e632ef90b034e10f
Nicholas Ruffing <nicholasruffing70@gmail.com>
2023-10-14 11:58:05 -0400

    add nav to dev app

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,8 @@
 <template>
+  <nav>
+    <router-link :to="{ name: 'home' }">Home</router-link>
+    <router-link :to="{ name: 'grid' }">Grid</router-link>
+  </nav>
   <router-view></router-view>
 </template>
 
@@ -13,13 +17,24 @@ export default defineComponent({
 <style>
 body, html {
   margin: 0;
-  background-color: rgb(40, 69, 97);
+  background-color: rgb(113, 115, 118);
 }
 
 body, html, main, #app {
   height: 100vh;
   min-height: 0;
   overflow: hidden;
+}
+
+#app {
+  display: grid;
+  grid-template-columns: 50px auto;
+}
+
+nav {
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
 }
 
 main {
