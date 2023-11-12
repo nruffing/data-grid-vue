977f073915f0421bb13f1848da3598529b750524
Nicholas Ruffing <nicholasruffing70@gmail.com>
2023-11-12 14:33:35 -0500

    add github buttons to navbar

--- vuepress/docs/.vuepress/client.ts
+++ vuepress/docs/.vuepress/client.ts
@@ -2,11 +2,15 @@ import { defineClientConfig } from '@vuepress/client'
 import { DataGridVue } from 'data-grid-vue'
 import DEMO from '../demo-data/DEMO_DATA'
 import constants from './constants'
+import Layout from './layouts/Layout.vue'
 
 export default defineClientConfig({
   enhance({ app, router, siteData }) {
     app.use(DataGridVue)
     app.provide('demo', DEMO)
     app.config.globalProperties.$dgv = constants
+  },
+  layouts: {
+    Layout,
   },
 })
