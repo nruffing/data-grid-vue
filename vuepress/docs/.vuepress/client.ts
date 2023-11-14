ffe7ba2e53e05ba067828b2425c921c70f440a59
Nicholas Ruffing <nicholasruffing70@gmail.com>
2023-11-13 23:30:31 -0500

    app insights

--- vuepress/docs/.vuepress/client.ts
+++ vuepress/docs/.vuepress/client.ts
@@ -1,18 +1,54 @@
 import { defineClientConfig } from '@vuepress/client'
+import { SidebarConfig } from '@vuepress/theme-default'
 import { DataGridVue } from 'data-grid-vue'
 import DEMO from '../demo-data/DEMO_DATA'
 import constants from './constants'
 import Layout from './layouts/Layout.vue'
 import { useThemeLocaleData } from '../../node_modules/@vuepress/theme-default/lib/client'
 import { RouteLocationNormalized, useRouter } from 'vue-router'
+import { ApplicationInsights } from '@microsoft/applicationinsights-web'
+import { ClickAnalyticsPlugin } from '@microsoft/applicationinsights-clickanalytics-js'
+import { generateW3CId } from '@microsoft/applicationinsights-core-js'
 
 let sideBarCache: 'auto' | false | SidebarConfig = false
+
+const clickPluginInstance = new ClickAnalyticsPlugin()
+const clickPluginConfig = {
+  autoCapture: true,
+  useDefaultContentNameOrId: true,
+  urlCollectQuery: true,
+  urlCollectHash: true,
+}
+const appInsights = new ApplicationInsights({
+  config: {
+    connectionString:
+      'InstrumentationKey=99a45096-c9c1-471c-ae4a-03ada9fe086f;IngestionEndpoint=https://eastus2-3.in.applicationinsights.azure.com/;LiveEndpoint=https://eastus2.livediagnostics.monitor.azure.com/',
+    extensions: [clickPluginInstance],
+    extensionConfig: {
+      [clickPluginInstance.identifier]: clickPluginConfig,
+    },
+  },
+})
 
 export default defineClientConfig({
   enhance({ app, router, siteData }) {
     app.use(DataGridVue)
     app.provide('demo', DEMO)
     app.config.globalProperties.$dgv = constants
+
+    if (import.meta.env.MODE === 'production') {
+      router.beforeEach((to: RouteLocationNormalized, from: RouteLocationNormalized) => {
+        appInsights.context.telemetryTrace.traceID = generateW3CId()
+        appInsights.context.telemetryTrace.name = to.name as string
+        appInsights.startTrackPage(to.fullPath)
+      })
+      router.afterEach((to: RouteLocationNormalized) => {
+        const url = location.protocol + '//' + location.host + to.fullPath
+        appInsights.stopTrackPage(to.fullPath, url)
+        appInsights.flush()
+      })
+      appInsights.loadAppInsights()
+    }
   },
   setup() {
     const themeLocale = useThemeLocaleData()
