3463b96fdcdb8a03020426a08e3e0057602ac840
Nicholas Ruffing <nicholasruffing70@gmail.com>
2023-11-24 16:53:06 -0500

    fix path casing

--- dev-app/src/router/index.ts
+++ dev-app/src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import GridView from '../Views/GridView.vue'
+import GridView from '../views/GridView.vue'
 
 const router = createRouter({
   history: createWebHistory(),
