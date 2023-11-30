361e95e5b582aba2f0b366ecc482ff98e4ac0818
Nicholas Ruffing <nicholasruffing70@gmail.com>
2023-11-30 18:25:08 -0500

    robots.txt and no index tags

--- vuepress/.vuepress/config.ts
+++ vuepress/.vuepress/config.ts
@@ -10,6 +10,10 @@ import { pwaPlugin } from '@vuepress/plugin-pwa'
 import { pwaPopupPlugin } from '@vuepress/plugin-pwa-popup'
 import { docsearchPlugin } from '@vuepress/plugin-docsearch'
 
+const hostname = 'https://datagridvue.com'
+const sitemapFilename = 'sitemap.xml'
+const sitemapUrl = path.join(hostname, sitemapFilename)
+
 const __dirname = getDirname(import.meta.url) // @/vuepress/docs/.vuepress
 const dgvStyleContents = await fs.readFile(path.resolve(__dirname, '../node_modules/data-grid-vue/dist/style.css'), 'utf8')
 const dgvStyleOverrideContents = await fs.readFile(path.resolve(__dirname, '../dgv-overrides.css'), 'utf8')
@@ -27,16 +31,46 @@ const cacheDir = path.resolve(__dirname, '../vuepress-cache')
 const tempDir = path.resolve(__dirname, '../vuepress-temp')
 const publicDir = path.resolve(__dirname, 'public')
 
+const skipIndexPaths = ['/404.html', '/shared/']
+function skipIndex(path: string | undefined) {
+  if (!path) {
+    return false
+  }
+  return skipIndexPaths.some(p => path.startsWith(p))
+}
+
+async function generateSitemap(app: App) {
+  const sitemap = new SitemapStream({ hostname })
+  const pages = app.pages.map(p => p.path).filter(p => !skipIndex(p))
+  const sitemapString = await streamToPromise(Readable.from(pages).pipe(sitemap)).then(data => data.toString())
+  fs.writeFile(path.resolve(publicDir, sitemapFilename), sitemapString)
+}
+
+async function generateRobotsTxt() {
+  const robotsTxt = `User-agent: *
+Allow: /
+
+SiteMap: ${sitemapUrl}`
+  fs.writeFile(path.resolve(publicDir, 'robots.txt'), robotsTxt)
+}
+
 export default defineUserConfig({
   lang: 'en-US',
-  title: 'Data Grid Vue',
+  title: 'Data Grid Vue | Native Vue3 Data Grid',
   description:
-    'Customizable native Vue3 data grid with very limited dependencies. Leverages a flat html structure and CSS grid to allow full layout control.',
+    'Customizable and accessible native Vue3 data grid with limited dependencies. Leverages a flat html structure and CSS grid to allow full layout control. MIT Licensed.',
   head: [
     ['link', { rel: 'icon', href: '/favicon.ico' }],
     ['link', { rel: 'manifest', href: '/manifest.webmanifest' }],
     ['style', { type: 'text/css' }, dgvStyleContents + '\n\n' + dgvStyleOverrideContents],
   ],
+  extendsPageOptions: (pageOptions, app) => {
+    if (skipIndex(pageOptions.path)) {
+      pageOptions.frontmatter = {
+        head: [['meta', { name: 'robots', content: 'noindex' }]],
+      }
+    }
+  },
   cache: cacheDir,
   temp: tempDir,
   define: constants,
@@ -142,13 +176,7 @@ export default defineUserConfig({
     }),
   ],
   async onInitialized(app) {
-    generateSitemap(app)
+    await generateSitemap(app)
+    await generateRobotsTxt()
   },
 })
-
-async function generateSitemap(app: App) {
-  const sitemap = new SitemapStream({ hostname: 'https://datagridvue.com' })
-  const pages = app.pages.map(p => p.path)
-  const sitemapString = await streamToPromise(Readable.from(pages).pipe(sitemap)).then(data => data.toString())
-  fs.writeFile(path.resolve(publicDir, 'sitemap.xml'), sitemapString)
-}
